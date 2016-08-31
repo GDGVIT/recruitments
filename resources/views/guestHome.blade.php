020eed391e3e76db1e3dc6ad394e97bcb84b38fc
Rishi Raj <rishiraj824@gmail.com>
2016-09-01 03:38:21 +0530

    guesthome done

--- resources/views/guestHome.blade.php
+++ resources/views/guestHome.blade.php
@@ -5,7 +5,26 @@
 
 @extends('layouts.afterloginnav')
 @section('content')
-  
+  <div class="container-fluid">
+  <div class="row">
+    <div class="col m6 l6 s10 push-l3 push-m3 push-s1 custom-pull-up">
+      <div class="center">
+        <div class="home-head">
+          Love Coding? Join GDG-VIT<br><span class="home-head-sub">Participate and compete with people across your campus</span>
+        </div>
+        <div class="row">
+          <div class="login-form">
+              <div class="col s12 m12 l12">
+              <h4>About GDG</h4>
+             	<p>Google Developers Groups was established in VIT on 28th July 2014. The group now has 60 Developers. Our agenda is to introduce new technologies to the students, develop new ideas and polish skills in every possible field including management our faculty coordinator is Prof .P Senthilnathan and has guided us along this journey.</p>
+             	<a href="http://gdgvitvellore.com" class="custom-button">View More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
 
      <div id="instructions-modal" class="modal">
         <div class="modal-content">
