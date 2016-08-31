1d6627e698f1a4eb33aa2d68a9eae80d7a6e456c
Rishi Raj <rishiraj824@gmail.com>
2016-09-01 04:10:27 +0530

    done

--- resources/views/User/Admin/showUserProfile.blade.php
+++ resources/views/User/Admin/showUserProfile.blade.php
@@ -7,8 +7,8 @@
         <div class="row">
 
             <div class="col m10 offset-m1">
-                <div class="question-card">
-                    <div class="card-title-main" style="    padding: 20px;">
+                <div class="card">
+                    <div class="card-title-main" style="    padding-left: 20px;padding-top:10px;padding-bottom:10px;">
                         <span class="card-title"><h4>{{$user->name}}</h4></span>
                     </div>
                     <hr>
@@ -28,21 +28,21 @@
                                 @endif
                             </ul>
                             @endforeach
-                        
-                        <h5>Why GDG :</h5><div style="text-overflow: ellipsis;overflow: hidden;min-height: 90px;"> {{$user->why_gdg}}</div>
-                        <h5>Experience :</h5><div style="text-overflow: ellipsis; overflow: hidden;min-height: 90px;"> {{$user->experience}}</div>
+                        <hr>
+                        <h5>Why GDG :</h5><div style="text-overflow: ellipsis;overflow: hidden;min-height: 90px;"> {{$user->why_gdg}}</div><hr>
+                        <h5>Experience :</h5><div style="text-overflow: ellipsis; overflow: hidden;min-height: 90px;"> {{$user->experience}}</div><hr>
                         @if($user->selected)
-                            <b style="color:green">Selected for the next round</b>
+                            <h5><b style="color:green">Selected for the next round</b></h5>
                         @else
-                            <b>Selected - No status</b>
+                            <h5><b>Selected - No status</b></h5>
                         @endif
                         <br>
                         <h5>Marks Awarded :<b> {{$user->marks}}</b></h5>
                     </div>
                     <div class="card-action">
-                        <a href="{{$user->linkedin}}"><i class="fa fa-linkedin fa-1x  " style="color:#5c6bc0 "></i></a>
-                        <a href="{{$user->github}}"><i class="fa fa-github fa-1x  " style="color:#5c6bc0 "></i></a>
-                        <a href="{{$user->behance}}"><i class="fa fa-behance fa-1x  " style="color:#5c6bc0 "></i></a>
+                        <a href="{{$user->linkedin}}"><i class="fa fa-linkedin fa-2x  " style="color:#5c6bc0 "></i></a>
+                        <a href="{{$user->github}}"><i class="fa fa-github fa-2x  " style="color:#5c6bc0 "></i></a>
+                        <a href="{{$user->behance}}"><i class="fa fa-behance fa-2x  " style="color:#5c6bc0 "></i></a>
                     </div>
                 </div>
 
--- resources/views/guestHome.blade.php
+++ resources/views/guestHome.blade.php
@@ -1,11 +1,78 @@
 @extends('layouts.app')
-<link rel="stylesheet" href="{{url('css/problems.css')}}">
-@extends('layouts.afterloginnav')
-<link href="css/modal.css" rel="stylesheet">
 
-@extends('layouts.afterloginnav')
 @section('content')
-  <div class="container-fluid">
+<div class="home-container">
+<div class="navbar-fixed">
+  <nav class="home-nav">
+    <div class="nav-wrapper">
+      <a href="{{ url('/') }}" class="hide-on-med-and-up nav-logo-mob"><img src="images/gdglogo.png"></a>
+       <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+      <div class="row">
+        <div class="col m10 l10 s12 push-l1 m1">
+          <ul class="left hide-on-med-and-down">
+          @if(\Illuminate\Support\Facades\Auth::user())
+            <li class="navbar-tabs nav-logo"><a href="{{url('/home')}}"><img src="images/gdglogo.png"></a></li>
+            
+          </ul>
+          <ul class="right hide-on-med-and-down">
+            <!-- Dropdown Trigger -->
+             <li><a class='dropdown-button problems-nav-options' href='#' data-activates='dropdown1'><b>{{\Illuminate\Support\Facades\Auth::user()->name}}</b><span class="caret"></span></a></li>
+            <li class="navbar-tabs"><a href="http://gdgvitvellore.com" target="_blank">Contact Us</a></li>
+           
+          </ul>
+          @else
+          <li class="navbar-tabs nav-logo"><a href="{{url('/')}}"><img src="images/gdglogo.png"></a></li>
+            
+          
+          <ul class="right hide-on-med-and-down">
+            <!-- Dropdown Trigger -->
+           
+            
+            <li class="navbar-tabs"><a href="http://gdgvitvellore.com" target="_blank">Contact Us</a></li>
+            <li class="navbar-tabs"><a href="{{url('/register')}}">Register</a></li>
+          </ul>
+           @endif
+        </div>
+      </div>
+
+      <ul class="side-nav" id="mobile-demo">
+           <li><a href="http://gdgvitvellore.com" target="_blank">Contact Us</a></li>
+           @if(\Illuminate\Support\Facades\Auth::user())
+
+          <li><a href="{{url('/home')}}">Home</a></li>
+
+          <li><a href="{{url('/problems')}}">Problems</a></li>
+          <li><a href="{{url('/user/profile')}}">Profile</a></li>
+          <li><a href="{{url('/logout')}}">Logout</a></li>
+
+           @else
+
+          <li><a href="{{url('/register')}}">Register</a></li>
+          <li><a href="{{url('/login')}}">Login</a></li>
+          @endif
+      </ul>
+    </div>
+  </nav>
+  <ul id='dropdown1' class='dropdown-content' style="min-width:120px;">
+                    <li><a href="/user/profile">Profile</a></li>
+                    <li><a href="{{url('/problems')}}">Problems</a></li>
+                    <li><a class="modal-trigger" href="#instructions-modal">Instructions</a></li>
+                    <li><a href="http://gdgvitvellore.com" target="_blank">Contact Us</a></li>
+                    <li><a href="/logout">Logout</a></li>
+    </ul>
+</div>
+<div class="home"></div>
+<br><br>
+<br><br>
+<br>
+<style type="text/css">
+  .buttons{
+    top:100px;
+    margin: 0 auto;
+    margin-top: 100px;
+  }
+</style>
+<div class="container-fluid">
   <div class="row">
     <div class="col m6 l6 s10 push-l3 push-m3 push-s1 custom-pull-up">
       <div class="center">
@@ -15,15 +82,19 @@
         <div class="row">
           <div class="login-form">
               <div class="col s12 m12 l12">
-              <h4>About GDG</h4>
-             	<p>Google Developers Groups was established in VIT on 28th July 2014. The group now has 60 Developers. Our agenda is to introduce new technologies to the students, develop new ideas and polish skills in every possible field including management our faculty coordinator is Prof .P Senthilnathan and has guided us along this journey.</p>
+              <div class="col s12 m12 l12">
+             	<p style="color:white">Google Developers Groups was established in VIT on 28th July 2014. The group now has 60 Developers. Our agenda is to introduce new technologies to the students, develop new ideas and polish skills in every possible field including management our faculty coordinator is Prof .P Senthilnathan and has guided us along this journey.</p>
              	<a href="http://gdgvitvellore.com" class="custom-button">View More</a>
+          </div>
+             
           </div>
         </div>
       </div>
     </div>
   </div>
 </div>
+</div>
+
 </div>
 
      <div id="instructions-modal" class="modal">
