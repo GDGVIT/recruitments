4239f98c91f08196ef173c90a4df987879c2c391
Rishi Raj <rishiraj824@gmail.com>
2016-09-01 21:51:36 +0530

    modal fixed

--- resources/views/guestHome.blade.php
+++ resources/views/guestHome.blade.php
@@ -87,7 +87,7 @@
              	<a href="http://gdgvitvellore.com" class="custom-button">View More</a>
           </div>
           <br><br>
-          <iframe style="margin-top:20px;" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fgdgvitvellore&tabs&width=340&height=154&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=true&appId=1668653593388184" width="340" height="154" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+          <iframe style="margin-top:20px;" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fgdgvitvellore&tabs&width=240&height=154&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=true&appId=1668653593388184" width="340" height="154" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
           </div>
         </div>
       </div>
