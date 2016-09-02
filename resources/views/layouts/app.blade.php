534d6a790e5d75dacfd435181f8dab28c0db5baf
Ashwini Purohit <ashwinipurohit@yahoo.in>
2016-09-02 16:49:26 +0530

    Google analytics added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,6 +54,16 @@ input[type="checkbox"].filled-in:checked + label:after {
     </style>
 </head>
 <body>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-83605119-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 <script  type="text/javascript" src="{{url('js/problemsLoader.js')}}"></script>
 <script  type="text/javascript" src="{{url('js/domainSelector.js')}}"></script>
 <script type="text/javascript" src="{{url('js/smoothscroll.js')}}"></script>
